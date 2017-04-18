f2c299dcc1b86ed9b9e2a62572c1f4ecc367d283
Vu Pham <vutpham@outlook.com>
2017-04-17 22:30:06 -0700

    Add and update w4d1

---
+++ W4D1/routes_and_controllers/app/models/artwork.rb
@@ -0,0 +1,13 @@
+class Artwork < ApplicationRecord
+  validates :title, uniqueness: { scope: :artist_id }
+
+  belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: 'User'
+
+  has_many :artwork_shares,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: 'ArtworkShare'
+end
