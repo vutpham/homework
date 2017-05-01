9f35e762a4449f5882bee77471e95409da95de79
Vu Pham <vutpham@outlook.com>
2017-04-30 19:18:24 -0700

    Sunday 4/30 commits

---
+++ W5D3/intro1.js
@@ -0,0 +1,122 @@
+// Throws an error if you define the same thing within the same scope
+// Looks like var ignores the block (?)
+// let vs var vs const
+
+function mysteryScoping1() {
+  var x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+// console.log(mysteryScoping1());
+
+function mysteryScoping2() {
+  const x = 'out of block';
+  if (true) {
+    const x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+// console.log(mysteryScoping2());
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+/* This throws an error message */
+// console.log(mysteryScoping3());
+
+function mysteryScoping4() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+// console.log(mysteryScoping4());
+
+function mysteryScoping5() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  x = 'out of block again';
+  console.log(x);
+}
+
+// console.log(mysteryScoping5());
+
+//MAD LIB FUNCTION -- Takes in VERB, ADJECTIVE, NOUN
+//ALL CAPS??
+function madLib(verb, adjective, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}.`;
+}
+
+// console.log(madLib('eat', 'delicious', 'food'));
+
+//isSubstring
+
+function isSubstring(searchString, subString) {
+  words = searchString.split(" ")
+
+  if (words.includes(subString)) {
+    return true;
+  }
+  return false;
+}
+
+// console.log(isSubstring("Jump for joy", "joys"));
+
+//FizzBuzz
+
+function fizzBuzz(array) {
+  let result = [];
+
+  array.forEach(el => {
+    if (el % 3 === 0 ^ el % 5 === 0){
+      result.push(el);
+    };
+  });
+  return result;
+}
+
+// console.log(fizzBuzz([1,2,3,15]));
+
+function isPrime(number) {
+  for (i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(isPrime(3548563));
+
+function sumOfNPrimes(n) {
+  let total = 0;
+  let count = 0;
+
+  for (i = 2; count < n; i++) {
+    if (isPrime(i)){
+      total += i;
+      count++;
+    }
+  }
+  return total;
+}
+
+// console.log(sumOfNPrimes(4));
---
+++ W5D3/intro2.js
@@ -0,0 +1,64 @@
+function titleize(names, callback) {
+  let titleized = names.map( name => `Mx. ${name} Jingleheimer Schmidt`)
+  callback(titleized)
+}
+
+// titleize(['Allen', 'Yong', 'Vu'], (names) => {
+//   names.forEach( name => console.log(name));
+// })
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+  return console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+}
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+}
+
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function() {
+  let max = this.tricks.length;
+  trickIndex = Math.floor(Math.random() * (max - 0))
+  console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+}
+
+Elephant.prototype.paradeHelper = function(name){
+  console.log(`${this.name} is trotting by!`);
+}
+
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+// console.log(charlie.paradeHelper());
+
+// herd.forEach( elephant => {
+//   console.log(elephant.paradeHelper);
+// })
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs please.";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+};
+
+bfast = dinerBreakfast();
+bfast('pretzels')
