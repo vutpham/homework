09ec9bdcf11b786274699ffee5c72b8b0255359d
Vu Pham <vutpham@outlook.com>
2017-09-20 16:36:55 -0700

    Remove localStorage

--- todos/frontend/store/store.js
+++ todos/frontend/store/store.js
@@ -3,9 +3,6 @@ import RootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
   const store = createStore(RootReducer, preloadedState);
-  store.subscribe(() => {
-    localStorage.state = JSON.stringify(store.getState());
-  });
   return store;
 };
 
