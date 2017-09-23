85d21e2df03d33da3b7e5b4ea448f4798f44aa13
Vu Pham <vutpham@outlook.com>
2017-09-22 22:48:03 -0700

    Add github grabber

--- test_project/src/component/menu.js
+++ test_project/src/component/menu.js
@@ -4,24 +4,6 @@ import Data from '../data/menu_data';
 import MenuItem from './menuItem';
 
 class Menu extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      shownContent: null
-    };
-
-    this.toggleContent = this.toggleContent.bind(this);
-  }
-
-  toggleContent(index) {
-    if (this.state.shownContent === index ) {
-      this.setState({shownContent: null});
-    } else {
-      this.setState({shownContent: index});
-    }
-  }
-
   showMenu() {
     return Data.map((menu, i) => {
       return (
@@ -30,8 +12,6 @@ class Menu extends Component {
           key={`menu_item_${i}`}
           header={ menu.header }
           content={ menu.content }
-          isShown={ this.state.shownContent === i }
-          onClick={ () => { this.toggleContent(i);} }
         />
       );
     });
--- test_project/src/component/menuItem.js
+++ test_project/src/component/menuItem.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class MenuItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shownContent: false
+    };
+
+    this.toggleContent = this.toggleContent.bind(this);
+  }
+
+  toggleContent() {
+    this.setState({shownContent: !this.state.shownContent});
+  }
 
   render() {
     let bullet = <i className="fa fa-angle-right" aria-hidden="true"></i>;
 
-    if (this.props.isShown) {
+    if (this.state.shownContent) {
       bullet = <i className="fa fa-angle-down" aria-hidden="true"></i>;
     }
 
@@ -13,9 +26,9 @@ class MenuItem extends Component {
     let { header, content } = this.props;
     return (
       <div>
-        <h2 onClick={this.props.onClick}>{ bullet }  { header }</h2>
+        <h2 onClick={this.toggleContent}>{ bullet }  { header }</h2>
         {
-          this.props.isShown &&
+          this.state.shownContent &&
           <p>{ content }</p>
         }
       </div>
